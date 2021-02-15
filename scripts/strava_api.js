a5e2c432bf68b6a30e6013a6fbd2649546e1e0a5
ScottTomassetti <scott_t_ct@hotmail.com>
2021-02-15 00:02:54 -0500

    Update per_page to 10 on index.html for table

--- scripts/strava_api.js
+++ scripts/strava_api.js
@@ -3,7 +3,7 @@ var myArray = []
 
 function getActivites(res){
 
-    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?per_page=10&access_token=${res.access_token}`
     fetch(activities_link)
         .then((res) => res.json())
         .then(function (data){
