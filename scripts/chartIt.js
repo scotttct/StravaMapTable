c5a87931d9284700a0ce00a90bdc152b01511857
ScottTomassetti <scott_t_ct@hotmail.com>
2021-02-22 22:10:56 -0500

    Update Chart Format and Options

--- scripts/chartIt.js
+++ scripts/chartIt.js
@@ -32,7 +32,7 @@ function getData(res){
                         label: 'Average Watts per ride',
                         data: myArray.map(actWatts => actWatts.average_watts),
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(66, 227, 245, 0.2)',
                             'rgba(54, 162, 235, 0.2)',
                             'rgba(255, 206, 86, 0.2)',
                             'rgba(75, 192, 192, 0.2)',
@@ -40,21 +40,21 @@ function getData(res){
                             'rgba(255, 159, 64, 0.2)'
                         ],
                         borderColor: [
-                            'rgba(255, 99, 132, 1)',
+                            'rgba(35, 134, 145, 1)',
                             'rgba(54, 162, 235, 1)',
                             'rgba(255, 206, 86, 1)',
                             'rgba(75, 192, 192, 1)',
                             'rgba(153, 102, 255, 1)',
                             'rgba(255, 159, 64, 1)'
                         ],
-                        borderWidth: 1
+                        borderWidth: 3
                     }]
                 },
                 options: {
                     scales: {
                         yAxes: [{
                             ticks: {
-                                beginAtZero: true
+                                beginAtZero: false
                             }
                         }]
                     }
